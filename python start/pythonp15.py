55f68d498e1b2201a41d32596034e3ef14479916
Minnl <635142748@qq.com>
2024-10-12 19:07:00 +0900

    24/10/12python

---
+++ python start/pythonp15.py
@@ -0,0 +1,10 @@
+print('hello"baba"');
+print("hello'baba'");
+print("he said \"Let\'s go!\"");
+print("hello \nbaba");
+print('''hello
+      baba
+      i
+      am
+      sb''');
+
--- test.py
+++
@@ -1,9 +0,0 @@
-print("hello");
-print("sb");
-print("abc");
-print("cvb");
-print("cvb");
-print(1+1);
-print("cvb");
-print("cvb");
-print(1+1);
